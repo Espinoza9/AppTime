527c7aae7a859a7b09fd63ca43fa400695783ff7
Espinoza9 <92704684+Espinoza9@users.noreply.github.com>
2022-11-20 22:35:21 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Search from './Search';
+
+function App() {
+  return (
+    <div className="App">
+      <Search placeholder ='Digite Aqui'/>
+  
+
+    </div>
+  );
+}
+export default App;
